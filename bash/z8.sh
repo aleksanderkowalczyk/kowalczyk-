27d2c704df7d9aeb5611dd7df7b3c4decadb137d
lukasz210933 <lukasz210933@wp.pl>
2020-01-02 09:35:46 +0100

    Select, case function

---
+++ bash/z8.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#definicja funkcji f1
+
+function f1() {
+echo -n "podaj nazwę użytkownika"
+
+function f1() {
+echo -n "podaj nazwę użytkownika"
+function f1() {
+echo -n "podaj nazwę użytkownika"
+#menu skryptu
+select opcja in f1 f2 f3 Quit 
+do
+    case ${opcja} in
+	"f1") echo "f1" ;;
+	"f2") echo "f2" ;;
+	"f3") echo "f3" ;;
+	"Quit") echo exit 0 ;;
+done
