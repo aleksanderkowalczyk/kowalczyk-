a5a2ec8f993f24b66d1c3ba72ed750475d01c60b
lukasz210933 <lukasz210933@wp.pl>
2020-01-09 09:40:58 +0100

    showusers, addusers

--- bash/manage_user.sh
+++ bash/manage_user.sh
@@ -1,51 +1,63 @@
 #!/bin/bash
 
-function loadUser() {
-    echo "loadUsers ..."
+#zmienna globalna
+#ładowanie danych z pliku
+function showUser() {
+    echo "LoadUsers ..."
+    #ładowanie danych z pliku
+    user_list=(`cat users.txt`)
+    echo "Lista:"
+    for(( i=0; i<=${#user_list[@]}; i++ ))
+    do
+	echo "<>${user_list[i]}"
+    done
 }
-
-
-function showUsers() {
-    echo "showUsers ..."
-}
-
 
 function addUsers() {
-    echo "addUsers ..."
+    echo "AddUsers ..."
+    for user in "${user_list[@]}"
+    do
+	echo ${user}
+    done
 }
 
 
 function delUsers() {
-    echo "delUsers ..."
+    echo "DelUsers ..."
 }
 
 
 function acceptRemoteLogin() {
-    echo "acceptRemoteLogin ..."
+    echo "AcceptRemoteLogin ..."
 }
 
 
 function demiedRemoteLogin() {
-    echo "demiedRemoteLogin ..."
+    echo "DemiedRemoteLogin ..."
 }
 
 function help() {
-cat << EndMessage
+cat << EndOfMessage
+    Opis opcji skryptu
+    ...................................
+    LU - ładowanie użytkowników z pliku
+    SU - Listowanie załadowanych użytkowników
 
+EndOfMessage
+}
 function quit {
     exit 0
 }
 
 #menu
-select option in LU SU AU DU ARL DRL HELP quit
+select option in SU AU DU ARL DRL HELP quit
 do
-    case $(option) in
-	 "LU") loadUsers ;;
-	 "SU") loadUsers ;;
-	 "AU") loadUsers ;;
-	 "DU") loadUsers ;;
-	 "ARL") loadUsers ;;
-	 "DRL") loadUsers ;;
+    case ${option} in
+	 "SU") showUsers ;;
+	 "AU") addUsers ;;
+	 "DU") delUsers ;;
+	 "ARL") acceptRemoteLogin ;;
+	 "DRL") deniedRemoteLogin ;;
 	 "HELP") help ;;
 	 "quit")quit ;;
     *)help
